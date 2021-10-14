3eb323001ee4654404a0f6494cbcb5f4e1c41095
Valentin LEROY <contact@vleroy.fr>
2021-10-14 10:14:43 +0200

    Disabling IP assignment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ async function main() {
 
     // Initialiser la connexion à Agones
     console.log(SIDECAR_PREFIX + 'Connecting to Agones...')
-    // await agonesSDK.connect();
+    await agonesSDK.connect()
     console.log(SIDECAR_PREFIX + '...connected to Agones')
 
     // Synchroniser les infos Agones avec la config du serveur
@@ -26,7 +26,7 @@ async function main() {
     const ip = gameServerInfos.status.address
     const port = gameServerInfos.status.portsList.find((p) => (p.name === 'game')).port
     const portTv = gameServerInfos.status.portsList.find((p) => (p.name === 'gotv')).port
-    process.env.IP = ip
+    // process.env.IP = ip
     process.env.PORT = port
     process.env.TV_PORT = portTv
 
@@ -36,7 +36,7 @@ async function main() {
     console.log(SIDECAR_PREFIX + 'Starting CSGO server on ' + ip + ':' + port + '...')
     const child = spawn(process.env.STEAM_DIR + '/start.sh')
 
-    child.stdout.setEncoding('utf8');
+    child.stdout.setEncoding('utf8')
     child.stdout.on('data', function(data) {
         console.log(CSGO_PREFIX + data)
 
@@ -48,16 +48,16 @@ async function main() {
             agonesSDK.ready()
             console.log(CSGO_PREFIX + 'CSGO SERVER IS READY')
         }
-    });
+    })
 
-    child.stderr.setEncoding('utf8');
+    child.stderr.setEncoding('utf8')
     child.stderr.on('data', function(data) {
         console.log(CSGO_PREFIX + 'ERROR | ' + data)
-    });
+    })
 
     child.on('close', function(code) {
         console.log(CSGO_PREFIX + 'EXIT CODE | ' + code)
-    });
+    })
 
     // Faire des health checks en boucle
     setInterval(doHealthChecks, 3000)
